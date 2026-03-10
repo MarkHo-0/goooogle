ff75babe24b3ab1591c7ba3dcbd8716e4de67050
Mark <s12059gsps@gmail.com>
2026-03-10 22:46:04 +0800

    Add initial project structure with Maven configuration and Spring Boot setup

---
+++ src/main/java/com/hkust/goooogle/GoooogleApplication.java
@@ -0,0 +1,13 @@
+package com.hkust.goooogle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoooogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoooogleApplication.class, args);
+	}
+
+}
